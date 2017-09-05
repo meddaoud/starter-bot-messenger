9511b30d9bc888eec47ed00fd945f6d4d11bbccc
meddaoud <31484452+meddaoud@users.noreply.github.com>
2017-09-05 11:48:55 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {}
 
 config.pageAccessToken = 'EAAV2SPNdsF0BAMinvoUkbVWZC7eJFaql8uoWZC28ia3tgXeEWelKhtVb1ickd6GpABMwHzg2h6HMyhTzcpXlACZCHxHyB2b8QVOgVIjpSgSjZCLLd2NBlFFZBHgO4MjP6eF54pn5voHRmqpCrYzR8fIuv3oICww1esBAvBWfTmAS7KCsnOzwO'
 config.validationToken = 'ABCD1234'
-config.recastToken = '7b38b88e159645c34bcdb90f77534738'
+config.recastToken = 'df85694e54584ec203d201b3e359aa9f'
 config.language = 'fr'
 
 module.exports = config
